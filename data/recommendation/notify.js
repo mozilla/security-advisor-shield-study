65e1a00f3ef94564002567206a6b8c6860deae21
casebenton <casebenton@gmail.com>
2016-08-15 12:47:38 -0700

    Use querySelectors, indent HTML, helper function for regex

--- data/recommendation/notify.js
+++ data/recommendation/notify.js
@@ -6,34 +6,38 @@ class Notify {
     });
   }
 
+  prettifyDate(ISODate) {
+    return ISODate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
   showAdvice(recs) {
     const date = new Date(recs.BreachDate);
     const year = date.getFullYear();
-    const victimCount = recs.PwnCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    document.getElementById('welcome').classList.remove('hidden');
-    document.getElementById('complete').classList.add('hidden');
-    document.getElementById('title').innerHTML = `Change your password for ${recs.Title}`;
-    const dateTag = document.getElementById('date');
+    const victimCount = this.prettifyDate(recs.PwnCount);
+    document.querySelector('#welcome').classList.remove('hidden');
+    document.querySelector('#complete').classList.add('hidden');
+    document.querySelector('#title').innerHTML = `Change your password for ${recs.Title}`;
+    const dateTag = document.querySelector('#date');
     dateTag.innerHTML = `This site was hacked in ${year}`;
-    document.getElementById('count').innerHTML = `This has affected ${victimCount} users`;
+    document.querySelector('#count').innerHTML = `This has affected ${victimCount} users`;
   }
   showSuccess() {
-    document.getElementById('welcome').classList.add('hidden');
-    document.getElementById('complete').classList.remove('hidden');
-    const domainH3 = document.getElementById('message');
+    document.querySelector('#welcome').classList.add('hidden');
+    document.querySelector('#complete').classList.remove('hidden');
+    const domainH3 = document.querySelector('#message');
     domainH3.innerHTML = 'We won\'t show this message anymore for this domain';
   }
 
   setup() {
-    const signupButton = document.getElementById('signup');
+    const signupButton = document.querySelector('#signup');
     signupButton.addEventListener('click', () => {
       self.port.emit('signup');
     });
-    const finishButton = document.getElementById('finished');
+    const finishButton = document.querySelector('#finished');
     finishButton.addEventListener('click', () => {
       this.showSuccess();
     });
-    const closeButton = document.getElementById('done');
+    const closeButton = document.querySelector('#done');
     closeButton.addEventListener('click', () => {
       self.port.emit('disableSite');
     });
