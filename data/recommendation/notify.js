53678df3313174e98e6c0faaa0cc8fc0337598db
casebenton <casebenton@gmail.com>
2016-08-15 11:31:15 -0700

    Panel provides domain-specific information
    The content script now processes domain-specific imformation from the API, such as the date of the breach, and displays this in the panel. The displayed data is basic now (only date and victim count), but it can easily be extended in the future to include more information.
    
    Closes #1

--- data/recommendation/notify.js
+++ data/recommendation/notify.js
@@ -1,14 +1,21 @@
 class Notify {
   start() {
     this.setup();
-    self.port.on('data', (/* recs */) => { // TODO: use rec data for customized panel info
-      this.showAdvice();
+    self.port.on('data', recs => {
+      this.showAdvice(recs);
     });
   }
 
-  showAdvice() {
+  showAdvice(recs) {
+    const date = new Date(recs.BreachDate);
+    const year = date.getFullYear();
+    const victimCount = recs.PwnCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     document.getElementById('welcome').classList.remove('hidden');
     document.getElementById('complete').classList.add('hidden');
+    document.getElementById('title').innerHTML = `Change your password for ${recs.Title}`;
+    const dateTag = document.getElementById('date');
+    dateTag.innerHTML = `This site was hacked in ${year}`;
+    document.getElementById('count').innerHTML = `This has affected ${victimCount} users`;
   }
   showSuccess() {
     document.getElementById('welcome').classList.add('hidden');
