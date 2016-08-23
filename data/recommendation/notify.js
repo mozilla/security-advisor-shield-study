d0a181c787f7dfae8f26977a681bf749afc3ad84
casebenton <casebenton@gmail.com>
2016-08-23 15:05:08 -0700

    update render function, fix callbacks

--- data/recommendation/notify.js
+++ data/recommendation/notify.js
@@ -4,7 +4,8 @@ class Notify {
   start() {
     this.target = this.createBase();
     document.body.appendChild(this.target);
-    const methodsToBind = ['handleEndWarning', 'handleEndSignup'];
+    const methodsToBind = ['handleNoAccount', 'handlePasswordChange', 'handleSignup',
+                           'handleNoThanks'];
     for (let key of methodsToBind) { // eslint-disable-line prefer-const
       this[key] = this[key].bind(this);
     }
@@ -12,7 +13,7 @@ class Notify {
       const date = new Date(recs.BreachDate);
       const year = date.getFullYear();
       this.render({
-        warn: true,
+        boxType: 'warn',
         site: recs.Title,
         year,
       });
@@ -28,14 +29,14 @@ class Notify {
 
   render(options) {
     const div = this.createBase();
-    if (options.warn) {
+    if (options.boxType === 'warn') {
       div.appendChild(this.createWarningBox(options.site, options.year));
-    } else { // !warn
-      if (options.changedPassword) {
-        div.appendChild(this.createPasswordChangedBox());
-      } else { // !changedPassword
-        div.appendChild(this.createNoAccountBox());
-      }
+    } else if (options.boxType === 'changedPassword') {
+      div.appendChild(this.createPasswordChangedBox());
+    } else if (options.boxType === 'noAccount') { // no account
+      div.appendChild(this.createNoAccountBox());
+    } else {
+      throw new Error(`Unrecognized boxType "${options.boxType}"`);
     }
     yo.update(this.target, div);
   }
@@ -66,8 +67,8 @@ class Notify {
   createWarningFooter() {
     return yo`
       <footer>
-        <div onclick=${this.handleEndWarning(true)}>I've changed my password</div>
-        <div onclick=${this.handleEndWarning(false)}>I don't have an account</div>
+        <div onclick=${this.handlePasswordChange}>I've changed my password</div>
+        <div onclick=${this.handleNoAccount}>I don't have an account</div>
       </footer>
     `;
   }
@@ -113,28 +114,39 @@ class Notify {
   createSignupFooter() {
     return yo`
       <footer>
-        <div onclick=${this.handleEndSignup(false)}>No, thanks</div>
-        <div onclick=${this.handleEndSignup(true)}>Use Firefox Sync</div>
+        <div onclick=${this.handleNoThanks}>No, thanks</div>
+        <div onclick=${this.handleSignup}>Use Firefox Sync</div>
       </footer>
     `;
   }
 
-  handleEndWarning(changedPassword) {
+  handleNoAccount() {
     if (!this.hasSync) {
       this.render({
-        warn: false,
-        changedPassword,
+        boxType: 'noAccount',
       });
-    } else {
+    } else { // !hasSync
       self.port.emit('disableSite');
     }
   }
 
-  handleEndSignup(wantsSync) {
-    self.port.emit('disableSite');
-    if (wantsSync) {
-      self.port.emit('signup');
+  handlePasswordChange() {
+    if (!this.hasSync) {
+      this.render({
+        boxType: 'changedPassword',
+      });
+    } else { // !hasSync
+      self.port.emit('disableSite');
     }
+  }
+
+  handleSignup() {
+    self.port.emit('disableSite');
+    self.port.emit('signup');
+  }
+
+  handleNoThanks() {
+    self.port.emit('disableSite');
   }
 }
 
