a6be0b68a2089642769a39a48f185cfa4aba3cf8
casebenton <casebenton@gmail.com>
2016-08-25 10:47:41 -0700

    add basic tests

--- test/test-index.js
+++ test/test-index.js
@@ -1,3 +1,28 @@
+const { Advisor } = require('..lib/Advisor.js');
+const { before, after } = require('sdk/test/utils');
+
+let advisor;
+
 exports['test main'] = (assert) => {
   assert.pass('Unit test running!');
 };
+
+exports['test main async'] = (assert, done) => {
+  assert.pass('async Unit test running!');
+  done();
+};
+
+exports['test advisor createPanel'] = (assert) => {
+  assert.ok((advisor.panel.isShowing === false), 'Panel created!');
+};
+
+before(exports, () => {
+  advisor = new Advisor();
+});
+
+require('sdk/test').run(exports);
+
+after(exports, () => {
+  advisor.destroy();
+  advisor = null;
+});
