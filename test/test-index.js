3515050895665f651b450d2f0cf64e06e85b0d4a
casebenton <casebenton@gmail.com>
2016-08-25 10:39:39 -0700

    remove async test

--- test/test-index.js
+++ test/test-index.js
@@ -1,8 +1,3 @@
 exports['test main'] = (assert) => {
   assert.pass('Unit test running!');
 };
-
-exports['test main async'] = (assert, done) => {
-  assert.pass('async Unit test running!');
-  done();
-};
